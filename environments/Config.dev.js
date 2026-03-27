ee53d574a1b5dfc0412db8d6cb91b878bfd0e209
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-03-27 00:02:13 -0600

    feat(UI): DataView Minimalist Polish & Dark Mode Theme Engine v1.0.82

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.0.67 - Build 260326.1340',
+    APP_VERSION: 'v1.0.82 - Build 260326.2355',
     // TODO: Reemplazar con el ID real de Desarrollo
     SPREADSHEET_ID_DB: '1tMyYBLPr0HJnJwbhFpCVoM4U8k1EVGycBDBjEnUGDts',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
