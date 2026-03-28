4f398952ef3323c5565f469fe8394a0390321a0a
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-03-28 11:57:32 -0600

    fix(s7.5): eradicate setTimeout in favor of deterministic ECharts finished lifecycle hook

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.0.89 - Sprint 5.1',
+    APP_VERSION: 'v1.1.3 - Sprint 7.4',
     SPREADSHEET_ID_DB: '1b15c9TylT8U5Z1eO8VjD0t-45YnI-K9LqV9x6G2K_pA',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
--- src/Global_Config.js
+++ src/Global_Config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.1.3 - Sprint 7.4',
+    APP_VERSION: 'v1.1.4 - Sprint 7.5',
     // TODO: Reemplazar con el ID real de Desarrollo
     SPREADSHEET_ID_DB: '1tMyYBLPr0HJnJwbhFpCVoM4U8k1EVGycBDBjEnUGDts',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
