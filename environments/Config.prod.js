af9a05d055d58c01255df5453f19c83e3cd29df4
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-03-27 21:05:22 -0600

    chore(config): bump prod version to v1.0.89 and add DB toggles

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,10 +1,12 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.0.88 - Sprint 4.3',
+    APP_VERSION: 'v1.0.89 - Sprint 5.1',
     SPREADSHEET_ID_DB: '1b15c9TylT8U5Z1eO8VjD0t-45YnI-K9LqV9x6G2K_pA',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
-    DEBUG: false
+    DEBUG: false,
+    useSheets: true,
+    useCloudDB: false
 };
 
 if (typeof module !== 'undefined') {
