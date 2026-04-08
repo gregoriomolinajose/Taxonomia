02feba1e8ab64c32a0ec594d13d9d709705c3418
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-08 00:23:13 -0600

    style(ui): Harmonize omnibar and dataview searchbar morphologies

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.4.0 - 2604080015',
+    APP_VERSION: 'v1.4.0 - 2604080022',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
