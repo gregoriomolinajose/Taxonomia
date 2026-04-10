cb5e6f0130e4420c8466e3366bc38046aa0c2d1c
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-10 14:23:06 -0600

    build(dev): record deployment of S30.4 v1.2.7 - 2604101422

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.7 - 2604101338',
+    APP_VERSION: 'v1.2.7 - 2604101422',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
