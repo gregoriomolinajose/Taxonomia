9cb780aa2386cf92efc2927d0fcc4468644f5d91
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-10 11:42:52 -0600

    build(deploy): push hotfix S30.7 versions

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.4 - 2604101137',
+    APP_VERSION: 'v1.2.4 - 2604101142',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.2.4 - 2604101138',
+    APP_VERSION: 'v1.2.4 - 2604101142',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
