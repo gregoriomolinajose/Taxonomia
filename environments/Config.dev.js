6a3856ecfe99fc8e8328127ed00102659fd858d4
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-10 21:18:29 -0600

    build: update config dev

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.7 - 2604102112',
+    APP_VERSION: 'v1.2.7 - 2604102113',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
