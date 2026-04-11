e07c3cfb262ee5b1f2c396afa1fe397deacd86de
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-10 20:27:33 -0600

    feat(S30.7): initialize story scope
    In scope:
    - Añadir explicit primaryKey en Schema_Engine.
    - Auditar consumo de primaryKey en ORM.
    
    Done when:
    - Todas las estructuras maestras cuenten con su delimitación de clave de manera formal.
    
    Co-Authored-By: Rai <rai@humansys.ai>

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.7 - 2604102016',
+    APP_VERSION: 'v1.2.7 - 2604102023',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
