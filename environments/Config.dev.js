dcb3defcae871679b49203557fc384a3bd1a7d13
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-11 20:47:14 -0600

    chore(deploy): update dev version stamp

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.7 - 2604111840',
+    APP_VERSION: 'v1.2.7 - 2604112044',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
