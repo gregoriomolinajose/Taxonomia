e16a1460735b6c47f3b55170277fe03321927904
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-04-22 11:41:56 -0600

    build(env): bump implementation version to v1.2.8 for prod deploy

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.2.8 - 2604220948',
+    APP_VERSION: 'v1.2.8 - 2604221140',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
