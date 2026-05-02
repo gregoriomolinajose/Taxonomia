9ed09ca591231f54a248005489baec87234d15d6
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-05-02 14:51:33 -0600

    chore: auto-update dev config and local context

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.19 - 2605012045',
+    APP_VERSION: 'v1.2.19 - 2605021446',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
