fab0d54743d2b2d030a7336980b69e6fb978caea
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-05-12 23:43:27 -0600

    fix(Index): inject UniversalFilter CSS and JS bundles into root template

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.2.8 - 2605122326',
+    APP_VERSION: 'v1.2.8 - 2605122335',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
