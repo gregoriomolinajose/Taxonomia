3eb08769c3dd22c3879c1af8919fa0b4e0c48aeb
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-05-16 01:42:55 -0600

    chore(deploy): push S56.1 to DEV and PROD environments

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.2.8 - 2605160052',
+    APP_VERSION: 'v1.2.8 - 2605160142',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
