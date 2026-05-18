201b4369b85591293df6fb00492f8e1c3d2ef3ab
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-05-18 13:08:33 -0600

    chore(release): bump dev and prod versions

--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.2.8 - 2605161041',
+    APP_VERSION: 'v1.2.8 - 2605181308',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
