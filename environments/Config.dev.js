e5a60b50f699dd3777ba06e5246f151a60aadcb8
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-05-21 19:11:36 -0600

    fix(s58.3): isolate topology depth validation by relation type and increase persona maxDepth

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.19 - 2605201748',
+    APP_VERSION: 'v1.2.19 - 2605202005',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app', '@coppel.com', '@bancoppel.com'],
     useSheets: true,
--- environments/Config.prod.js
+++ environments/Config.prod.js
@@ -1,6 +1,6 @@
 // src/Config.prod.js
 const CONFIG = {
-    APP_VERSION: 'v1.2.8 - 2605201755',
+    APP_VERSION: 'v1.2.8 - 2605202011',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@coppel.com', '@bancoppel.com', '@bellfy.app', '@gmail.com'],
     ENV: 'production',
