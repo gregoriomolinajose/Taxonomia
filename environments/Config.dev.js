7b42182481ee941c477cf0db8edc4e5e7735b3e0
Gregorio Molina <gregoriomolinajose@gmail.com>
2026-05-29 12:26:03 -0600

    docs(s68): add retrospective

--- environments/Config.dev.js
+++ environments/Config.dev.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    APP_VERSION: 'v1.2.19 - 2605281918',
+    APP_VERSION: 'v1.2.19 - 2605291225',
     SPREADSHEET_ID_DB: '',
     ALLOWED_DOMAINS: ['@gmail.com', '@bellfy.app'],  // Dev only — dominios del tenant se configuran via Adapter_Config
     useSheets: true,
